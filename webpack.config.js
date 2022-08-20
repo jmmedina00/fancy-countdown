c94ed2fc759299e9c20c9eafb28fad6124ada060
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-20 13:04:23 +0200

    build: add Bootstrap to bundle properly

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,28 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
