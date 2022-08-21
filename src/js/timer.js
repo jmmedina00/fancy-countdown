5dec4d780a0f1684c6c8d417ea6fe9da50b03cb8
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-21 12:16:15 +0200

    refactor: polish base aspect, rearrange elements

--- src/js/timer.js
+++ src/js/timer.js
@@ -4,12 +4,18 @@ export const getTimerComponent = () => {
   const units = ["Minutes", "Seconds"];
   const unitComponents = units.map((unit) => getTimeUnitComponent(unit));
 
+  const timerWrapper = document.createElement("div");
+  timerWrapper.classList.add("container-sm");
+
   const timerComponent = document.createElement("div");
   timerComponent.classList.add("row");
+  timerComponent.classList.add("flex-nowrap");
 
   for (const component of unitComponents) {
     timerComponent.appendChild(component);
   }
 
-  return timerComponent;
+  timerWrapper.appendChild(timerComponent);
+
+  return timerWrapper;
 };
