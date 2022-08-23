ae82d9786d676d8c661f31a871278acf6ca50e37
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-23 14:03:49 +0200

    refactor: convert timer wrapper to HTML

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,24 +1,18 @@
+import timerSrc from "../html/timer-wrapper.html";
+import { getComponent } from "./component";
 import { getTimeUnitComponent } from "./time-unit";
 
 export const getTimerComponent = () => {
+  const wrappedTimerComponent = getComponent(timerSrc);
+  const timerContainer =
+    wrappedTimerComponent.querySelector(".align-self-center");
+
   const units = ["Minutes", "Seconds"];
   const unitComponents = units.map((unit) => getTimeUnitComponent(unit));
 
-  const timerWrapper = document.createElement("div");
-  timerWrapper.classList.add("container-sm");
-  timerWrapper.classList.add("flex-grow-1");
-  timerWrapper.classList.add("d-flex");
-
-  const timerComponent = document.createElement("div");
-  timerComponent.classList.add("row");
-  timerComponent.classList.add("flex-grow-1");
-  timerComponent.classList.add("align-self-center");
-
   for (const component of unitComponents) {
-    timerComponent.appendChild(component);
+    timerContainer.appendChild(component);
   }
 
-  timerWrapper.appendChild(timerComponent);
-
-  return timerWrapper;
+  return wrappedTimerComponent;
 };
