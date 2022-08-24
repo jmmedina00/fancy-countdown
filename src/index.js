74c21cbb584329c458120842143daeb03727c1e4
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-24 12:57:06 +0200

    feat: implement proper timebar animation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import "./style.scss";
 import * as bootstrap from "bootstrap";
 import { addToBody } from "./js/component";
-import { timeBarComponent } from "./js/time-bar";
+import { getTimeBarComponent } from "./js/time-bar";
 import { getTimerComponent } from "./js/timer";
 import { provideTimers } from "./js/time-service";
 
 console.log("Hello World!");
-const { stopTimer, unitTimers } = provideTimers(140);
+const { stopTimer, unitTimers, remainingTime } = provideTimers(140);
 
-const timeBar = timeBarComponent.cloneNode(true);
+const timeBar = getTimeBarComponent(140, remainingTime);
 addToBody(timeBar);
 
 const timer = getTimerComponent(unitTimers);
 addToBody(timer);
-
-setTimeout(() => {
-  timeBar.style.width = "30%";
-}, 2000);
--- src/js/time-bar.js
+++ src/js/time-bar.js
@@ -1,4 +1,19 @@
 import timeBarSrc from "../html/time-bar.html";
 import { getComponent } from "./component";
+import { map } from "rxjs";
 
-export const timeBarComponent = getComponent(timeBarSrc);
+export const getTimeBarComponent = (baseTime, remainingTime) => {
+  const component = getComponent(timeBarSrc);
+
+  remainingTime.pipe(map((t) => t / 1000)).subscribe({
+    next: (t) => {
+      const percent = ((t / baseTime) * 100).toFixed(2);
+      component.style.width = `${percent}%`;
+    },
+    complete: () => {
+      component.style.width = "0";
+    },
+  });
+
+  return component;
+};
