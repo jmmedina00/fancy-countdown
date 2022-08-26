044241d4bd9bba5d23275e32037a21f0de8ec678
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-26 13:01:16 +0200

    feat: implement debounced range value for maximum

--- src/js/assembler/test-time-unit.js
+++ src/js/assembler/test-time-unit.js
@@ -1,10 +1,11 @@
+import { debounceTime } from "rxjs";
 import { getTestRangeComponentAndObservable } from "../view/test-range";
 
 export const getTestTimeUnitComponents = () => {
   const { component, observable: rangeObservable } =
     getTestRangeComponentAndObservable();
 
-  rangeObservable.subscribe((x) => console.log(x));
+  rangeObservable.pipe(debounceTime(1000)).subscribe((x) => console.log(x));
 
   return [component];
 };
