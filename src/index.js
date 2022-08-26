61f85e21eca93d8fa9edfe1cb36bd1d54c2c28fc
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-26 11:14:47 +0200

    feat: add remaining screen decision logic

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import "./style.scss";
 import * as bootstrap from "bootstrap";
 import { addToBody, getComponent } from "./js/util/component";
 import testSrc from "./html/test.html";
+import timeFormSrc from "./html/time-form.html";
 import { getCountdownComponents } from "./js/assembler/countdown";
 
 const decideComponents = () => {
   const params = new URL(document.documentURI).searchParams;
 
-  if (!params.has("time") || Number.isNaN(+params.get("time"))) {
+  if (params.has("test")) {
     return [getComponent(testSrc)];
+  }
+
+  if (!params.has("time") || Number.isNaN(+params.get("time"))) {
+    return [getComponent(timeFormSrc)];
   }
 
   const time = +params.get("time");
