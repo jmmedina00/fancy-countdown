bae7ff532eea64ee6e1cdd87ab7a094df7c3a76a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-26 12:43:54 +0200

    feat: implement range component behavior and observable

--- src/js/assembler/test-time-unit.js
+++ src/js/assembler/test-time-unit.js
@@ -1,7 +1,10 @@
 import { getTestRangeComponentAndObservable } from "../view/test-range";
 
 export const getTestTimeUnitComponents = () => {
-  const { component } = getTestRangeComponentAndObservable();
+  const { component, observable: rangeObservable } =
+    getTestRangeComponentAndObservable();
+
+  rangeObservable.subscribe((x) => console.log(x));
 
   return [component];
 };
--- src/js/view/test-range.js
+++ src/js/view/test-range.js
@@ -1,6 +1,22 @@
+import { fromEvent, merge, map } from "rxjs";
 import testRangeSrc from "../../html/test-range.html";
 import { getComponent } from "../util/component";
 
 export const getTestRangeComponentAndObservable = () => {
-  return { component: getComponent(testRangeSrc), observable: null };
+  const component = getComponent(testRangeSrc);
+  const range = component.querySelector("#maximumRange");
+  const currentValueLabel = component.querySelector("#currentValue");
+
+  const refreshValue = (value) => {
+    currentValueLabel.innerText = value;
+  };
+
+  const observable = merge(
+    fromEvent(window, "load"),
+    fromEvent(range, "input")
+  ).pipe(map(() => range.value));
+
+  observable.subscribe(refreshValue);
+
+  return { component, observable };
 };
