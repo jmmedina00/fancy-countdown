23635284ae7cb8f030c35cdb9cac1f9a3e1c37b7
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-08-26 13:35:16 +0200

    feat: add time unit with placeholder observable to test screen

--- src/js/assembler/test-time-unit.js
+++ src/js/assembler/test-time-unit.js
@@ -1,11 +1,28 @@
-import { debounceTime } from "rxjs";
+import { merge, debounceTime, fromEvent, of, timer, delay } from "rxjs";
+import { getComponent } from "../util/component";
 import { getTestRangeComponentAndObservable } from "../view/test-range";
+import timerSrc from "../../html/timer-wrapper.html";
+import { getTimeUnitComponent } from "../view/time-unit";
 
 export const getTestTimeUnitComponents = () => {
   const { component, observable: rangeObservable } =
     getTestRangeComponentAndObservable();
 
+  const wrappedTimerComponent = getComponent(timerSrc);
+  const timerContainer =
+    wrappedTimerComponent.querySelector(".align-self-center");
+
   rangeObservable.pipe(debounceTime(1000)).subscribe((x) => console.log(x));
 
-  return [component];
+  const mockedTimeObservable = fromEvent(document, "click");
+
+  const timeUnitComponent = getTimeUnitComponent([
+    "Test",
+    merge(of(5), timer(1000)).pipe(delay(500)), //Placeholder observable
+    5,
+  ]);
+
+  timerContainer.appendChild(timeUnitComponent);
+
+  return [component, wrappedTimerComponent];
 };
