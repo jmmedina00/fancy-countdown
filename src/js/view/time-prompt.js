2155a9d5962b41e1421c6ace3eb6268feb4770fd
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-06 11:33:04 +0200

    feat: add self-validation to time inputs

--- src/js/view/time-prompt.js
+++ src/js/view/time-prompt.js
@@ -1,14 +1,24 @@
+import { fromEvent } from "rxjs";
 import timePromptSrc from "../../html/time-form.html";
 import timeInputSrc from "../../html/time-input.html";
 import { getComponent } from "../util/component";
 
-const getTimeInput = ([label = "", maximum = 0]) => {
+const getTimeInputComponent = ([label = "", maximum = 0]) => {
+  const isCurrentValueValid = (value) =>
+    !Number.isNaN(+value) ? +value >= 0 && +value <= maximum : false;
+
   const timeInputComponent = getComponent(timeInputSrc);
   const timeInputNode = timeInputComponent.querySelector(".time-input");
   const timeLabel = timeInputComponent.querySelector(".time-label");
 
   timeInputNode.max = maximum;
   timeLabel.innerText = label;
+
+  fromEvent(timeInputNode, "input").subscribe(() =>
+    isCurrentValueValid(timeInputNode.value)
+      ? timeInputNode.classList.remove("is-invalid")
+      : timeInputNode.classList.add("is-invalid")
+  );
 
   return timeInputComponent;
 };
@@ -24,7 +34,7 @@ export const getTimePromptComponent = () => {
   const timePromptContainer = timePromptComponent.querySelector(".time-prompt");
 
   for (const unit of Object.entries(units)) {
-    const inputComponent = getTimeInput(unit);
+    const inputComponent = getTimeInputComponent(unit);
     timePromptContainer.appendChild(inputComponent);
   }
 
