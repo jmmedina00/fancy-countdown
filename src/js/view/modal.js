0f514c2528d3e06a44a85b9ea3bcbe6367623555
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-07 13:10:23 +0200

    feat: implement adding additional buttons to modal

--- src/js/view/modal.js
+++ src/js/view/modal.js
@@ -1,14 +1,28 @@
 import modalTemplateSrc from "../../html/modal-template.html";
+import modalCloseButtonSrc from "../../html/modal-close-button.html";
 import { getComponent } from "../util/component";
 import { Modal } from "bootstrap";
 
-export const getModalComponent = (message = "") => {
+export const getModalComponent = (message = "", links = {}) => {
   const baseModal = getComponent(modalTemplateSrc);
+  const closeButton = getComponent(modalCloseButtonSrc);
   const modalBody = baseModal.querySelector(".modal-body");
+  const modalFooter = baseModal.querySelector(".modal-footer");
 
   const messageParagraph = document.createElement("p");
   messageParagraph.innerHTML = message;
+
+  const additionalLinks = Object.entries(links).map(([label, url]) => {
+    const element = document.createElement("a");
+    element.classList.add("btn", "btn-secondary");
+    element.href = url;
+    element.innerHTML = label;
+
+    return element;
+  });
+
   modalBody.replaceChildren(messageParagraph);
+  modalFooter.replaceChildren(...additionalLinks, closeButton);
 
   const modalComponent = new Modal(baseModal, { backdrop: "static" });
   return modalComponent;
