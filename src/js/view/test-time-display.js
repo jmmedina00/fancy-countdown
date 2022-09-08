ebfa58a6e280015f01491ef71b8520b5bb619314
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-08 11:54:20 +0200

    fix: prevent discarded mocked timers from further triggering

--- src/js/view/test-time-display.js
+++ src/js/view/test-time-display.js
@@ -1,19 +1,32 @@
-import { debounceTime, delay, fromEvent, map, merge, of } from "rxjs";
+import {
+  debounceTime,
+  delay,
+  fromEvent,
+  map,
+  merge,
+  of,
+  takeUntil,
+} from "rxjs";
 import timerSrc from "../../html/timer-wrapper.html";
 import { getComponent } from "../util/component";
 import { getTimeUnitComponent } from "./time-unit";
-
-const getMockedTimeObservable = (maximum = 1) =>
-  merge(of(1).pipe(delay(500)), fromEvent(document, "click")).pipe(
-    map((_, index) => +maximum - (index % (+maximum + 1)))
-  );
 
 export const getTestTimeDisplayComponent = (timeUnitReplaceObservable) => {
   const wrappedTimerComponent = getComponent(timerSrc);
   const timerContainer =
     wrappedTimerComponent.querySelector(".align-self-center");
 
-  timeUnitReplaceObservable.pipe(debounceTime(1000)).subscribe((maximum) => {
+  const debouncedReplaceObservable = timeUnitReplaceObservable.pipe(
+    debounceTime(1000)
+  );
+
+  const getMockedTimeObservable = (maximum = 1) =>
+    merge(of(1).pipe(delay(500)), fromEvent(document, "click")).pipe(
+      map((_, index) => +maximum - (index % (+maximum + 1))),
+      takeUntil(debouncedReplaceObservable)
+    );
+
+  debouncedReplaceObservable.subscribe((maximum) => {
     const timeUnitComponent = getTimeUnitComponent([
       "Test",
       getMockedTimeObservable(maximum),
